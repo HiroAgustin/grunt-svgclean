8ce1cfc6a13a976c2529acecc72a3ae695d50e95
HiroAgustin <hiroagustin@gmail.com>
2014-10-14 11:24:38 -0400

    almost there

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,47 +12,42 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     jshint: {
+
       all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
+        'Gruntfile.js'
+      , 'tasks/*.js'
+      , '<%= nodeunit.tests %>'
+      ]
+
+    , options: {
         jshintrc: '.jshintrc'
       }
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    svgclean: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
 
+    // Before generating any new files, remove any previously-created files.
+  , clean: {
+      tests: ['tmp']
+    }
+
+    // Configuration to be run (and then tested).
+  , svgclean: {
+
+      dist: {
+
+        options: {}
+
+      , files: {
+          'tmp/head.svg': 'test/images/head.svg'
+        }
+      }
+    }
+
+    // Unit tests.
+  , nodeunit: {
+      tests: ['test/*_test.js']
+    }
   });
 
   // Actually load this plugin's task(s).
--- tasks/svgclean.js
+++ tasks/svgclean.js
@@ -5,46 +5,31 @@
  * Copyright (c) 2014 HiroAgustin
  * Licensed under the MIT license.
  */
+;(function (svgclean)
+{
+  'use strict';
 
-'use strict';
+  module.exports = function (grunt)
+  {
+    grunt.registerMultiTask('svgclean', 'Clean svg', function ()
+    {
+      var src = ''
+        , dest = '';
 
-module.exports = function(grunt) {
+      this.files.forEach(function (file)
+      {
+        src = file.src[0];
+        dest = file.dest;
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+        if (!grunt.file.exists(src))
+          grunt.log.warn('Source file "' + src + '" not found.');
+        
+        svgclean(src, dest);
 
-  grunt.registerMultiTask('svgclean', 'Clean svg', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
+        // Print a success message.
+        grunt.log.writeln('File "' + dest + '" created.');
+      });
     });
+  };
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
-  });
-
-};
+}(require('svgclean')));
